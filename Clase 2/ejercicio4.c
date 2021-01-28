f0a3f83fdb0a25c2fbfd6c4fc7a0993e27a94563
RichyFlame <3094047891210@ingenieria.usac.edu.gt>
2021-01-28 08:53:46 -0600

    Clase 2

---
+++ Clase 2/ejercicio4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int x = 33;
+    int y;
+    int *p;
+    
+    p = &x;
+    
+    printf("El valor de p es %d \n", *p);
+    
+    y = *p + 10;
+    
+    printf("el valor de y es %d", y);
+    
+    return 0;
+}
